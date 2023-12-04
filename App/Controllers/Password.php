e888d1e6c4bc0101d3945116a3a5d457cdf65363
Lukasz Kublik <lukasz.kublik.programista@gmail.com>
2023-12-04 08:28:39 +0100

    Update reset a forgotten password

--- App/Controllers/Password.php
+++ App/Controllers/Password.php
@@ -34,4 +34,74 @@ class Password extends \Core\Controller
 
         View::renderTemplate('Password/reset_requested.html');
     }
+
+    /**
+     * Show the reset password form
+     *
+     * @return void
+     */
+    public function resetAction()
+    {
+        $token = $this->route_params['token'];
+
+        $user = $this->getUserOrExit($token);
+
+        View::renderTemplate('Password/reset.html', [
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * Reset the user's password
+     *
+     * @return void
+     */
+    public function resetPasswordAction()
+    {
+        // Check if the "token" key is present in the $_POST array
+        if (!isset($_POST['token'])) {
+        // Handle the error, e.g., redirect to an error page
+        // You can also log the error for debugging purposes
+        die('Token not found in POST data');
+    }
+        
+        $token = $_POST['token'];
+
+        $user = $this->getUserOrExit($token);
+
+        if ($user->resetPassword($_POST['password'])) {
+
+            View::renderTemplate('Password/reset_success.html');
+
+        } else {
+
+            View::renderTemplate('Password/reset.html', [
+                'token' => $token,
+                'user' => $user
+            ]);
+        }
+    }
+
+    /**
+     * Find the user model associated with the password reset token, or end the request with a message
+     *
+     * @param string $token Password reset token sent to user
+     *
+     * @return mixed User object if found and the token hasn't expired, null otherwise
+     */
+    protected function getUserOrExit($token)
+    {
+        $user = User::findByPasswordReset($token);
+
+        if ($user) {
+
+            return $user;
+
+        } else {
+
+            View::renderTemplate('Password/token_expired.html');
+            exit;
+
+        }
+    }
 }
